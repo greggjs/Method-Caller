adc18410514595d2837734df4afdd3d2e85a2543
Jake Gregg <greggjs@miamioh.edu>
2014-01-21 14:18:44 -0500

    initial commit

--- Method-Caller/main.cpp
+++ Method-Caller/main.cpp
@@ -7,12 +7,25 @@
 //
 
 #include <iostream>
+#include <dlfcn.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    std::cout << "Hello, World!\n";
+    if (argc < 3) {
+        cout << "Invalid number of args. Need filename and method." << endl;
+        return 0;
+    }
+    
+    //ifstream in (argv[1]);
+    
     return 0;
 }
 
