5dcadf968be0cbe0a0bb36586c4c30fb6f53be70
Jake Gregg <greggjs@miamioh.edu>
2014-01-21 14:05:08 -0500

    Initial Commit

---
+++ Method-Caller/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Method-Caller
+//
+//  Created by Jake Gregg on 1/21/14.
+//  Copyright (c) 2014 Parallel and Cloud Computing laboratory. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
